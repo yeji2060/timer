f13e1b655c945ffc98c9d602d251078513145a46
Yeji Ha <yeji206000@gmail.com>
2023-11-22 13:16:13 -0800

    timer function added

--- timer1.js
+++ timer1.js
@@ -1,11 +1,10 @@
-const array = process.argv.slice(2);
-
-const sortedArray = array.sort((a, b) => a - b)
+const array =  process.argv.slice(2).sort((a, b) => a - b);
 
 for(let i = 0; i < array.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, array[i] * 1000);
-
+  if (array[i] >= 0) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, array[i] * 1000);  
+  }
 }
 
